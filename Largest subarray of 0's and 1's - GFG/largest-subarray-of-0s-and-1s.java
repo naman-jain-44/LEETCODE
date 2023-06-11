9e83f2217ea0b7364242a531b4108e2d94ab3703
Naman Jain <100465390+naman-jain-44@users.noreply.github.com>
2023-06-11 14:52:57 +0530

    Added solution - LeetHub

---
+++ Largest subarray of 0's and 1's - GFG/largest-subarray-of-0s-and-1s.java
@@ -0,0 +1,96 @@
+//{ Driver Code Starts
+import java.util.Scanner;
+import java.util.*;
+import java.lang.*;
+import java.util.HashMap;
+
+class Largest_Subarray
+{
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        while (T > 0)
+        {
+            int N = sc.nextInt();
+            int a[] = new int[N];
+            for (int i = 0; i < N; i++) 
+                a[i] = sc.nextInt();
+            
+
+            Solution g = new Solution();
+            int n = g.maxLen(a, a.length);
+
+            System.out.println(n);
+
+            T--;
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+
+
+class Solution {
+
+    // arr[] : the input array containing 0s and 1s
+    // N : size of the input array
+    
+    // return the maximum length of the subarray
+    // with equal 0s and 1s
+    int maxLen(int[] arr, int n)
+    {
+        // Your code here
+          HashMap<Integer, Integer> hM
+            = new HashMap<Integer, Integer>();
+
+        // Initialize sum of elements
+        int sum = 0;
+
+        // Initialize result
+        int max_len = 0;
+        int ending_index = -1;
+        int start_index = 0;
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = (arr[i] == 0) ? -1 : 1;
+        }
+
+        // Traverse through the given array
+
+        for (int i = 0; i < n; i++) {
+            // Add current element to sum
+
+            sum += arr[i];
+
+            // To handle sum=0 at last index
+
+            if (sum == 0) {
+                max_len = i + 1;
+                ending_index = i;
+            }
+
+            // If this sum is seen before,
+            // then update max_len if required
+            if (hM.containsKey(sum)) {
+                if (max_len < i - hM.get(sum)) {
+                    max_len = i - hM.get(sum);
+                    ending_index = i;
+                }
+            }
+            else // Else put this sum in hash table
+                hM.put(sum, i);
+        }
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = (arr[i] == -1) ? 0 : 1;
+        }
+
+        int end = ending_index - max_len + 1;
+     //   System.out.println(end + " to " + ending_index);
+
+        return max_len;
+    }
+}
