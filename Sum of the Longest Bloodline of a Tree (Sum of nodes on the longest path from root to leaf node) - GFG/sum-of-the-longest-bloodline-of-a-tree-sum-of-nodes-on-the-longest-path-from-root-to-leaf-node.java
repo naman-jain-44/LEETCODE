8cc87419f448727cd779893db3d64d3ade02a661
Naman Jain <100465390+naman-jain-44@users.noreply.github.com>
2023-06-16 09:49:55 +0530

    Added solution - LeetHub

---
+++ Sum of the Longest Bloodline of a Tree (Sum of nodes on the longest path from root to leaf node) - GFG/sum-of-the-longest-bloodline-of-a-tree-sum-of-nodes-on-the-longest-path-from-root-to-leaf-node.java
@@ -0,0 +1,158 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+import java.lang.*; 
+
+
+class Node {
+    int data;
+    Node left, right;
+    
+    public Node(int data){
+        this.data = data;
+    }
+}
+
+class GFG {
+    
+    
+    static Node buildTree(String str){
+        
+        if(str.length()==0 || str.charAt(0)=='N'){
+            return null;
+        }
+        
+        String ip[] = str.split(" ");
+        // Create the root of the tree
+        Node root = new Node(Integer.parseInt(ip[0]));
+        // Push the root to the queue
+        
+        Queue<Node> queue = new LinkedList<>(); 
+        
+        queue.add(root);
+        // Starting from the second element
+        
+        int i = 1;
+        while(queue.size()>0 && i < ip.length) {
+            
+            // Get and remove the front of the queue
+            Node currNode = queue.peek();
+            queue.remove();
+                
+            // Get the current node's value from the string
+            String currVal = ip[i];
+                
+            // If the left child is not null
+            if(!currVal.equals("N")) {
+                    
+                // Create the left child for the current node
+                currNode.left = new Node(Integer.parseInt(currVal));
+                // Push it to the queue
+                queue.add(currNode.left);
+            }
+                
+            // For the right child
+            i++;
+            if(i >= ip.length)
+                break;
+                
+            currVal = ip[i];
+                
+            // If the right child is not null
+            if(!currVal.equals("N")) {
+                    
+                // Create the right child for the current node
+                currNode.right = new Node(Integer.parseInt(currVal));
+                    
+                // Push it to the queue
+                queue.add(currNode.right);
+            }
+            i++;
+        }
+        
+        return root;
+    }
+    public static void inorder(Node root){
+        if(root == null)
+            return;
+        
+        inorder(root.left);
+        System.out.print(root.data+" ");
+        inorder(root.right);
+    }
+    
+	public static void main (String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t=Integer.parseInt(br.readLine());
+		while(t-- > 0){
+		    String s = br.readLine();
+	    	Node root = buildTree(s);
+		    Solution obj = new Solution();
+		    int res = obj.sumOfLongRootToLeafPath(root);
+		    System.out.println(res);
+		}
+	}
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+/*
+node class of binary tree
+class Node {
+    int data;
+    Node left, right;
+    
+    public Node(int data){
+        this.data = data;
+    }
+}
+*/
+class Solution{
+    int max ;
+    public int sumOfLongRootToLeafPath(Node root)
+    {
+        //code here
+        max=0;
+        int h=recurse(root);
+        
+        int max=re(root,h);
+        
+        return max;
+    }
+    
+    public int recurse(Node root){
+        if(root==null){
+            return 0;
+        }
+        return 1+Math.max(recurse(root.left),recurse(root.right));
+    }
+    
+    public int re(Node root , int h){
+        if(root==null){
+            return 0;
+        }
+        
+        if(root.left==null && root.right==null && h==1){
+            return root.data;
+        }
+         if(root.left==null && root.right==null && h>1){
+            return 0;
+        }
+        
+        int l =re(root.left,h-1);
+        int r =re(root.right,h-1);
+        int ans=0;
+        if(l!=0 || r!=0){
+            ans =root.data+Math.max(l,r);
+            
+        }
+        
+        return ans;
+    }
+    
+    
+}
