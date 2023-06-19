9c734d6f65c81c0762c20ea277ae3dc2f54f9dba
Naman Jain <100465390+naman-jain-44@users.noreply.github.com>
2023-06-19 19:46:41 +0530

    Added solution - LeetHub

---
+++ Parenthesis Checker - GFG/parenthesis-checker.java
@@ -0,0 +1,85 @@
+//{ Driver Code Starts
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+class Driverclass
+{
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        
+        //Reading total number of testcases
+        int t= sc.nextInt();
+        
+        while(t-- >0)
+        {
+            //reading the string
+            String st = sc.next();
+            
+            //calling ispar method of Paranthesis class 
+            //and printing "balanced" if it returns true
+            //else printing "not balanced"
+            if(new Solution().ispar(st) == true)
+                System.out.println("balanced");
+            else
+                System.out.println("not balanced");
+        
+        }
+    }
+}
+// } Driver Code Ends
+
+
+
+class Solution
+{
+    //Function to check if brackets are balanced or not.
+    static boolean ispar(String x)
+    {
+        Stack<Character> s = new Stack<>();
+        // add your code here
+        for(int i=0;i<x.length();i++){
+            if(x.charAt(i)=='('||x.charAt(i)=='['||x.charAt(i)=='{'){
+                s.push(x.charAt(i));
+            }else{
+                if(x.charAt(i)==')'){
+                    if(s.size()==0){
+                        return false;
+                    }
+                    else if(s.peek()=='('){
+                        s.pop();
+                    }else{
+                        return false;
+                    }
+                    
+                }else if(x.charAt(i)==']'){
+                     if(s.size()==0){
+                        return false;
+                    }
+                    else if(s.peek()=='['){
+                        s.pop();
+                    }else{
+                        return false;
+                    }
+                    
+                    
+                }else{
+                     if(s.size()==0){
+                        return false;
+                    }
+                    else if(s.peek()=='{'){
+                        s.pop();
+                    }else{
+                        return false;
+                    }
+                    
+                    
+                }
+            }
+            
+        }
+        
+        return s.size()==0;
+    }
+}
